342c8e26ec1f72ae261e0fd65e9a0117bb907bf5
slstevens <sean.stevens.123@gmail.com>
2014-10-04 00:00:49 +0100

    Did the Game Class wooooo

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,25 @@
-class Game
+class Game 
+
+	attr_accessor :player_one, :player_two
+
+	def initialize
+		@player_one = nil
+		@player_two = nil
+	end
+	
+	def winner
+		return player_one.name if player_one_wins?
+		player_two.name
+	end
+
+	def player_one_wins?
+		(player_one.choice == :rock && player_two.choice == :scissors)||
+		(player_one.choice == :paper && player_two.choice == :rock)||
+		(player_one.choice == :scissors && player_two.choice == :paper)
+	end
+
+	def draw?
+		player_one.choice === player_two.choice
+	end
+	
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,60 @@
 require 'game'
 
 describe Game do
+
+	let(:player_one) { double :player, name: "Sean"}
+	let(:player_two) { double :player, name: "Enemy"}
+	let(:game) 		 { Game.new }
 	
-	it "should have objects (rock, paper, scissors) for the player" do
-	end	
+	context 'when playing' do
+
+		before(:each) {
+			game.player_one = player_one
+			game.player_two = player_two
+		}
+
+		it "Player 1 chooses rock and WINS if Player 2 chooses scissors" do
+			allow(player_one).to receive(:choice).and_return(:rock)
+			allow(player_two).to receive(:choice).and_return(:scissors)
+			expect(game.winner).to eq player_one.name 
+		end
+		
+		it "Player 1 chooses paper and WINS if Player 2 chooses rock" do
+			allow(player_one).to receive(:choice).and_return(:paper)
+			allow(player_two).to receive(:choice).and_return(:rock) 
+			expect(game.winner).to eq player_one.name
+		end
+
+		it "Player 1 chooses scissors and WINS if Player 2 chooses paper" do
+			allow(player_one).to receive(:choice).and_return(:scissors)
+			allow(player_two).to receive(:choice).and_return(:paper) 
+			expect(game.winner).to eq player_one.name
+		end
+
+		it "Player 1 and Player 2 DRAW if they choose the same object" do
+			allow(player_one).to receive(:choice).and_return(:scissors)
+			allow(player_two).to receive(:choice).and_return(:scissors) 
+			expect(game.draw?).to eq true
+		end
+
+		it "Player 1 chooses rock and LOSES if Player 2 chooses scissors" do
+			allow(player_one).to receive(:choice).and_return(:rock)
+			allow(player_two).to receive(:choice).and_return(:paper)
+			expect(game.winner).to eq player_two.name 
+		end
+		
+		it "Player 1 chooses paper and LOSES if Player 2 chooses rock" do
+			allow(player_one).to receive(:choice).and_return(:paper)
+			allow(player_two).to receive(:choice).and_return(:scissors) 
+			expect(game.winner).to eq player_two.name
+		end
+
+		it "Player 1 chooses scissors and LOSES if Player 2 chooses paper" do
+			allow(player_one).to receive(:choice).and_return(:scissors)
+			allow(player_two).to receive(:choice).and_return(:rock) 
+			expect(game.winner).to eq player_two.name
+		end
+
+	end
 
 end
